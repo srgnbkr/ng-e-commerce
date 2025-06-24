67c40c6c6e535d05eb002b60fb347b447e32c6cb
Sergen <cuseg09@gmail.com>
2025-06-24 18:21:11 +0300

    feat: update app structure with topbar and footer components, enhance app configuration, and apply background styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+import { TopbarComponent } from './core/layouts/components/app.topbar';
+import { ProductService } from './features/products/services/product.service';
+import { PaginatorModule } from 'primeng/paginator';
+import { FooterComponent } from './core/layouts/components/app.footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TopbarComponent,FooterComponent, RouterModule, PaginatorModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  public productSercice = inject(ProductService);
+  products = this.productSercice.products;
+  totalRecords = this.productSercice.productsTotal;
+  first = this.productSercice.skip();
+  rows = this.productSercice.limit();
+
   title = 'ng-e-commerce';
+
+  onPageChange(event: any): void {
+    this.first = event.first;
+    this.rows = event.rows;
+    this.productSercice.setPage(this.first, this.rows);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import Aura from '@primeng/themes/aura';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
