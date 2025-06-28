6d305b22ce21e8c03404bf6304df00c3d6dc62b3
Sergen <cuseg09@gmail.com>
2025-06-28 18:08:56 +0300

    feat: add LandingComponent with HTML template and routing for landing page

---
+++ src/app/features/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  imports: [RouterLink],
+  templateUrl: './landing.component.html',
+})
+export class LandingComponent {
+
+}
